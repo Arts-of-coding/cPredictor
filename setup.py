3190295131f7fbeb5b628987a3d2e6a930b83506
Arts-of-coding <julian.armando.arts@gmail.com>
2023-06-01 16:08:31 +0200

    fixed dependencies and local test of PYPI

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
 	name='cmaclp',
-	version='0.0.1',
+	version='0.0.6',
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
@@ -15,8 +15,8 @@ setup(
             'SVM_prediction=cmaclp.SVM_prediction:main'
         ]
     },
-    install_requires=['python-build','h5py','numpy','pandas','scikit-learn','scanpy','rpy2','importlib-resources','pytest-cov'],
-    package_data={"cmaclp.data": ["*.csv", "*.h5ad"]},
+    install_requires=['setuptools<=57.5.0','wheel','python-build','h5py','numpy>=1.23.3, <1.24','pandas>=1.4.4','scikit-learn','scanpy>=1.9.1','rpy2','importlib-resources','pytest-cov'],
+    #package_data={"cmaclp.data": ["*.csv", "*.h5ad"]},
     long_description=long_description,
     long_description_content_type='text/markdown'
 )
