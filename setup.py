f31e16378e72a6568b87410e5d64d3b5a164f2f9
Arts-of-coding <julian.armando.arts@gmail.com>
2023-06-02 15:19:42 +0200

    upgrade version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
 	name='cmaclp',
-	version='0.0.6',
+	version='0.0.7',
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
