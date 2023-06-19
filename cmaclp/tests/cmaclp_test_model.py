5a1fd1712ba1a4f9f8e982d003da4eb1660531f1
Arts-of-coding <julian.armando.arts@gmail.com>
2023-06-19 17:05:01 +0200

    Added DH logging and upgrade to v0.1.0

--- cmaclp/tests/cmaclp_test_model.py
+++ cmaclp/tests/cmaclp_test_model.py
@@ -6,6 +6,7 @@ import pandas as pd
 import scanpy as sc
 import time as tm
 import seaborn as sns
+import rpy2.robjects as robjects
 import matplotlib
 import matplotlib.pyplot as plt
 from sklearn.svm import LinearSVC
@@ -19,13 +20,47 @@ from sklearn.metrics import precision_score
 from scanpy import read_h5ad
 from importlib.resources import files
 
-print("import performance function")
+print("Import performance function")
 from cmaclp.SVM_prediction import SVM_performance
 
 reference = "data/cma_meta_atlas.h5ad"
 labels = "data/training_labels_meta.csv"
 outdir = "test_output/"
-cmaclp_version = "0.0.10"
+cmaclp_version = "0.1.0"
 
 metrics = SVM_performance(reference_H5AD=reference,LabelsPath=labels,OutputDir=outdir)
-print(metrics)
+
+print("Setup ML tracking packages")
+import dagshub
+import mlflow
+
+# Set environments and passwords
+DAGSHUB_USER_NAME = 'Arts-of-coding'
+DAGSHUB_TOKEN =  os.environ['DH_key']
+DAGSHUB_REPO_OWNER = 'Arts-of-coding'
+MLFLOW_EXPERIMENT_NAME = 'Cornea'
+DAGSHUB_REPO_NAME='cmaclp'
+Upload_type="GHA"
+SVM_model="SVMrej"
+
+os.environ['MLFLOW_TRACKING_USERNAME'] = DAGSHUB_USER_NAME
+os.environ['MLFLOW_TRACKING_PASSWORD'] = DAGSHUB_TOKEN
+os.environ['MLFLOW_TRACKING_URI'] = f'https://dagshub.com/{DAGSHUB_REPO_OWNER}/{DAGSHUB_REPO_NAME}.mlflow'
+mlflow.set_tracking_uri(os.environ['MLFLOW_TRACKING_URI'])
+os.environ['MLFLOW_EXPERIMENT_NAME'] = MLFLOW_EXPERIMENT_NAME
+
+print("Upload metrics to dagshub for model tracking")
+
+# Create new experiment if it does not exist yet otherwise add to the current experiment
+try:
+    experiment_id = mlflow.create_experiment(MLFLOW_EXPERIMENT_NAME)
+except:
+    pass
+
+mlflow.start_run(run_name=str(f'{MLFLOW_EXPERIMENT_NAME}_full_{Upload_type}'),experiment_id=experiment_id,
+                 tags={"version": str(cmaclp_version),"model": str(SVM_model)})
+mlflow.log_metric("weighted_F1_score", metrics[0])
+mlflow.log_metric("weighted_accuracy_score", metrics[1])
+mlflow.log_metric("weighted_precision_score", metrics[2])
+
+mlflow.end_run()
