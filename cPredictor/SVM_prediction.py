a7d03326041a5c8b130ac089f220880eccc24370
J Arts <78762941+Arts-of-coding@users.noreply.github.com>
2024-01-26 14:47:28 +0100

    Update SVM_prediction.py check labels_train

--- cPredictor/SVM_prediction.py
+++ cPredictor/SVM_prediction.py
@@ -187,7 +187,7 @@ def SVM_predict(reference_H5AD, query_H5AD, LabelsPath, OutputDir, rejected=Fals
     
     labels_train = pd.read_csv(LabelsPath, header=0,index_col=None, sep=',')
     labels_train = labels_train.values.ravel()
-
+    print(labels_train)
     # Load in the test data from on disk incrementally
     with pa.memory_map('data_test.arrow', 'rb') as source:
         data_test = pa.ipc.open_file(source).read_all()
@@ -200,7 +200,6 @@ def SVM_predict(reference_H5AD, query_H5AD, LabelsPath, OutputDir, rejected=Fals
     data_test = cpredictor.preprocess_data_test(data_test)
     
     if rejected is True:
-        print(data_test)
         cpredictor.fit_and_predict_svmrejection(labels_train, Threshold_rej, OutputDir, data_train, data_test)
         cpredictor.save_results(rejected)
         
@@ -444,12 +443,12 @@ def SVM_performance(reference_H5AD, LabelsPath, OutputDir, rejected=True, Thresh
         data_train = data_train_processed[train_ind[i]]
         data_test = data_train_processed[test_ind[i]]
         labels_train = y[train_ind[i]]
+        print(labels_train)
         y_test = y[test_ind[i]]
 
         if rejected is True:
             start = tm.time()
             SVM_type = "SVMrejected"
-            print(data_test)
             predicted, prob = cpredictorperf.fit_and_predict_svmrejection(labels_train, 
                                                                           Threshold_rej, 
                                                                           OutputDir, 
