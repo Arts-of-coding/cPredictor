5dd51544ac848de54ac01ffd4443d09bd4deb6e1
J Arts <78762941+Arts-of-coding@users.noreply.github.com>
2024-02-05 09:36:51 +0100

    Update SVM_prediction.py max_iter_2500

--- cPredictor/SVM_prediction.py
+++ cPredictor/SVM_prediction.py
@@ -27,7 +27,7 @@ import logging
 class CpredictorClassifier():
     def __init__(self, Threshold_rej, rejected, OutputDir):
         self.scaler = MinMaxScaler()
-        self.Classifier = LinearSVC(dual = False, random_state = 42, class_weight = 'balanced', max_iter = 5000)
+        self.Classifier = LinearSVC(dual = False, random_state = 42, class_weight = 'balanced', max_iter = 2500)
         self.threshold = Threshold_rej
         self.rejected = rejected
         self.output_dir = OutputDir
