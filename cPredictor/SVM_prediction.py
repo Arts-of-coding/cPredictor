61a53836bb986eddfb3a02c9d20d05b6480d5bda
J Arts <78762941+Arts-of-coding@users.noreply.github.com>
2024-05-08 14:00:56 +0200

    Update SVM_prediction.py font fix not needed

--- cPredictor/SVM_prediction.py
+++ cPredictor/SVM_prediction.py
@@ -31,7 +31,6 @@ import logging
 import pickle
 import joblib
 import json
-matplotlib.rc('pdf', fonttype=42)
 
 class CpredictorClassifier():
     def __init__(self, Threshold_rej, rejected, OutputDir):
